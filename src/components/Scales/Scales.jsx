7c311a4d09e0e5719ea81b0f25c7ce87f69db720
frexxx-7 <borzenkokiril9@gmail.com>
2023-11-25 00:01:08 +0300

    fixed slider

---
+++ src/components/Scales/Scales.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Scales.module.scss'
+
+const Scales = () => {
+  return (
+    <div>Scales</div>
+  )
+}
+
+export default Scales
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,37 +5,17 @@ import { Carousel } from 'react-carousel-minimal';
 const Main = () => {
   const data = [
     {
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
-      caption: "San Francisco"
+      image: "./slider-1.jpeg",
+      caption: "Премиум весы высокого качества"
     },
     {
-      image: "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
+      image: "./slider-2.jpg",
+      caption: "Лучшие цены"
     },
-    {
-      image: "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
-    },
-    {
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
-    },
-    {
-      image: "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
-    },
-    {
-      image: "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
-    },
-    {
-      image: "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
-    },
-    {
-      image: "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
-    },
-    {
-      image: "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
-    }
   ];
 
   const captionStyle = {
-    fontSize: '2em',
+    fontSize: '3rem',
     fontWeight: 'bold',
   }
   const slideNumberStyle = {
@@ -43,35 +23,39 @@ const Main = () => {
     fontWeight: 'bold',
   }
   return (
-    <div className="App">
-      <div style={{ textAlign: "center" }}>
-        <div style={{
-          padding: "0 20px"
-        }}>
-          <Carousel
-            data={data}
-            time={2000}
-            width="850px"
-            height="500px"
-            captionStyle={captionStyle}
-            radius="10px"
-            slideNumberStyle={slideNumberStyle}
-            captionPosition="bottom"
-            automatic={true}
-            dots={true}
-            pauseIconColor="white"
-            pauseIconSize="40px"
-            slideBackgroundColor="darkgrey"
-            slideImageFit="cover"
-            thumbnailWidth="100px"
-            style={{
-              textAlign: "center",
-              maxWidth: "850px",
-              maxHeight: "500px",
-              margin: "40px auto",
-            }}
-          />
+    <div className={classes.main}>
+      <div className={classes.sliderContainer}>
+        <div style={{ textAlign: "center" }}>
+          <div style={{
+            padding: "0 20px"
+          }}>
+            <Carousel
+              data={data}
+              time={2000}
+              width="1380px"
+              height="500px"
+              captionStyle={captionStyle}
+              slideNumberStyle={slideNumberStyle}
+              captionPosition="center"
+              dots={true}
+              pauseIconColor="white"
+              pauseIconSize="40px"
+              slideBackgroundColor="darkgrey"
+              slideImageFit="cover"
+              thumbnailWidth="100px"
+              style={{
+                textAlign: "center",
+                maxWidth: "1380px",
+                maxHeight: "500px",
+                margin: "20px auto",
+              }}
+            />
+          </div>
         </div>
+      </div>
+
+      <div className={classes.newScales}>
+              
       </div>
     </div>
   )
