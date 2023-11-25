835a065d136d3163e9627ea19df4d95e187d1ca7
frexxx-7 <borzenkokiril9@gmail.com>
2023-11-25 13:15:39 +0300

    added our advantages

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import classes from './Main.module.scss'
 import { Carousel } from 'react-carousel-minimal';
 import Scales from '../../components/Scales/Scales';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboard } from '@fortawesome/free-regular-svg-icons';
 
 const Main = () => {
   const data = [
@@ -64,6 +66,84 @@ const Main = () => {
             <Scales />
             <Scales />
             <Scales />
+          </div>
+        </div>
+
+        <div className={classes.howChoiceScale}>
+          <div className={classes.howChoiceScaleContainer}>
+            <h2>Как выбрать весы?</h2>
+            <p>Выбор правильных весов может быть сложной задачей, учитывая разнообразие моделей и функций, предлагаемых на рынке. Однако, с учетом нескольких ключевых факторов, можно сделать более осознанный выбор, который будет соответствовать вашим потребностям. В этой статье мы рассмотрим несколько важных аспектов, которые следует учитывать при выборе весов.</p>
+            <button className={classes.howChoiceButton}>Узнать подробнее</button>
+          </div>
+        </div>
+
+        <div className={classes.newScales}>
+          <div className={classes.headNewScales}>
+            <p className={classes.headNewScalesP}>Настольные весы</p>
+          </div>
+          <div className={classes.contentNewScales}>
+            <Scales />
+            <Scales />
+            <Scales />
+            <Scales />
+            <Scales />
+          </div>
+        </div>
+        <div className={classes.newScales}>
+          <div className={classes.headNewScales}>
+            <p className={classes.headNewScalesP}>Напольные весы</p>
+          </div>
+          <div className={classes.contentNewScales}>
+            <Scales />
+            <Scales />
+            <Scales />
+            <Scales />
+            <Scales />
+          </div>
+        </div>
+
+        <div className={classes.ourAdvantages}>
+          <div className={classes.headNewScales}>
+            <p className={classes.headNewScalesP}>Наши плюсы</p>
+          </div>
+
+          <div className={classes.ourAdvantagesContainer}>
+
+            <div className={classes.ourAdnvantagesCard}>
+              <div className={classes.ourAdvantagesIcon}>
+                <FontAwesomeIcon icon={faClipboard} />
+              </div>
+              <div className={classes.ourAdvantagesTitle}>
+                <p>Широкий ассортимент</p>
+              </div>
+              <div className={classes.ourAdvantagesDescription}>
+                <p>Мы закупаем фрукты и овощи по всему миру – от Эквадора до Китая. Более 100 видов электронных весов и фруктов представлены на наших прилавках.</p>
+              </div>
+            </div>
+
+            <div className={classes.ourAdnvantagesCard}>
+              <div className={classes.ourAdvantagesIcon}>
+                <FontAwesomeIcon icon={faClipboard} />
+              </div>
+              <div className={classes.ourAdvantagesTitle}>
+                <p>Широкий ассортимент</p>
+              </div>
+              <div className={classes.ourAdvantagesDescription}>
+                <p>Мы закупаем фрукты и овощи по всему миру – от Эквадора до Китая. Более 400 сортов овощей и фруктов представлены на наших прилавках.</p>
+              </div>
+            </div>
+            <div className={classes.ourAdnvantagesCard}>
+              <div className={classes.ourAdvantagesIcon}>
+                <FontAwesomeIcon icon={faClipboard} />
+              </div>
+              <div className={classes.ourAdvantagesTitle}>
+                <p>Широкий ассортимент</p>
+              </div>
+              <div className={classes.ourAdvantagesDescription}>
+                <p>Мы закупаем фрукты и овощи по всему миру – от Эквадора до Китая. Более 100 видов весов представлены на наших прилавках.</p>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
