0b445ed0c07e14e876675199e6a807c59bdf48a8
frexxx-7 <borzenkokiril9@gmail.com>
2024-01-04 23:19:53 +0300

    added save basket info to db

--- src/components/Scales/Scales.jsx
+++ src/components/Scales/Scales.jsx
@@ -3,13 +3,81 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import classes from './Scales.module.scss'
 import { faCartShopping, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router-dom'
+import { useStateContext } from '../../context/ContextProvider'
+import { useEffect } from 'react'
+import axiosCLient from '../../axios.client'
 
 const Scales = ({ dataScale }) => {
-
+  const { user } = useStateContext()
   const [visivlePulsMinusButton, setVisiblePlusMinusButton] = useState(false)
   const [count, setCount] = useState(1)
-
+  const [basketScaleInfo, setBasketScaleInfo] = useState({})
   const navigate = useNavigate()
+
+  const loadInfoScaleInBasket = () => {
+    const payload = {
+      idUser: user.id,
+      idScale: dataScale.id,
+    }
+    axiosCLient.post(`/infoScaleScaleInBasket`, payload)
+      .then(({ data }) => {
+        if (data) {
+          setBasketScaleInfo(data ? data.basket[0] : {})
+        }
+      })
+      .catch(({ response }) => {
+        console.log(response);
+      })
+  }
+  const addedEditScaleToDB = () => {
+    const payload = {
+      idUser: user.id,
+      idScale: dataScale.id,
+      count: count,
+      purchased: false
+    }
+    Object.keys(basketScaleInfo || {}).length == 0 ?
+      axiosCLient.post('/addScaleToBasket', payload)
+        .then(({ data }) => {
+          if (data) {
+            setBasketScaleInfo(data ? data.basket[0] : {})
+          }
+        })
+        .catch(err => {
+          console.log(err.response);
+          const response = err.response
+          if (response && response.status === 422) {
+            setErrors(response.data.errors)
+          }
+        })
+      :
+      axiosCLient.post(`/editScaleToBasket/${basketScaleInfo.id}`, payload)
+        .then(({ data }) => {
+          if (data) {
+            console.log(data);
+            setBasketScaleInfo(data.basket[0])
+          }
+        })
+        .catch(err => {
+          console.log(err.response);
+          const response = err.response
+          if (response && response.status === 422) {
+            setErrors(response.data.errors)
+          }
+        })
+  }
+  console.log(basketScaleInfo);
+  useEffect(() => {
+    if (Object.keys(user).length != 0) {
+      loadInfoScaleInBasket()
+    }
+  }, [user])
+  useEffect(() => {
+    if (Object.keys(basketScaleInfo || {}).length != 0) {
+      setCount(basketScaleInfo.count)
+      setVisiblePlusMinusButton(true)
+    }
+  }, [basketScaleInfo])
   return (
     <div className={classes.ScaleMain}>
       <div className={classes.scaleProductContent}>
@@ -29,15 +97,18 @@ const Scales = ({ dataScale }) => {
               if (count <= 1) {
                 setVisiblePlusMinusButton(false)
                 setCount(1)
+                addedEditScaleToDB()
               }
             }}>
             <FontAwesomeIcon icon={faMinus} />
           </button>
           <button className={classes.scaleButton} onClick={
             () => {
-              if(visivlePulsMinusButton)
-                navigate('/basket')
+              if (visivlePulsMinusButton)
+                return navigate('/basket')
               setVisiblePlusMinusButton(true)
+
+              addedEditScaleToDB()
             }
           }>
             {
@@ -54,7 +125,12 @@ const Scales = ({ dataScale }) => {
                 </>
             }
           </button>
-          <button className={classes.plusButton} style={{ display: visivlePulsMinusButton ? "block" : " none" }} onClick={() => setCount(count + 1)}>
+          <button className={classes.plusButton} style={{ display: visivlePulsMinusButton ? "block" : " none" }}
+            onClick={() => {
+              setCount(count + 1)
+              addedEditScaleToDB()
+            }
+            }>
             <FontAwesomeIcon icon={faPlus} />
           </button>
         </div>
